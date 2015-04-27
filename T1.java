1ca82c49d1c7fb1697b0f27defeb0a6ad9bca4ff
yanbasic <yanbasic@gmail.com>
2015-04-27 17:20:50 +0800

    sdfsdf

--- T1.java
+++ T1.java
@@ -3,6 +3,7 @@ imports="
     org.talend.core.model.process.INode 
     org.talend.core.model.process.ElementParameterParser 
     org.talend.designer.codegen.config.CodeGeneratorArgument
+    org.talend.designer.codegen.config.CodeGeneratorArgument
 " 
 %>
 <%@ include file="Log4jFileUtil.javajet"%>
